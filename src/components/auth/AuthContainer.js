746ddf78dda782b098a52ae0eadecf7dda462562
keipy27 <kevinandris27@gmail.com>
2023-08-01 10:38:15 +1200

    login Js code added, Auth css code implemented and as well as global style code

---
+++ src/components/auth/AuthContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./AuthContainer.scss";
+import Login from './Login';
+
+const AuthContainer = () => {
+  return (
+    <div>
+        <Login/>
+    </div>
+  )
+}
+
+export default AuthContainer
