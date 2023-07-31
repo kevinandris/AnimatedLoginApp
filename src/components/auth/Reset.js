8c19d6f9efe46e4675ab412d9c769582bf1a5916
keipy27 <kevinandris27@gmail.com>
2023-08-01 11:19:17 +1200

    Reset.js file code added, its style also added with media screen effect

--- src/components/auth/Reset.js
+++ src/components/auth/Reset.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import resetImg from "../../assets/forgot.svg"
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Reset = () => {
   return (
-    <div>
-        <h1>Reset</h1>
+    <div className='main-container --flex-center'>
+
+        <div className="form-container reset">
+            <form className="--form-control">
+                <h2 className='--color-danger --text-center' >Reset Password</h2>
+                <input type="email" className='--width-100' placeholder='Email'/>
+                <button className="--btn --btn-primary --btn-block">Reset Password</button>
+                <span className='--text-sm --block --text-center'>We will send you a reset link!!! {" "}</span>
+
+                <div className="close">
+                    <AiOutlineClose color='red'/>
+                </div>
+            </form>
+        </div>
+
+        <div className="img-container">
+            <img src={resetImg} alt="login" />
+        </div>
+       
     </div>
   )
 }
